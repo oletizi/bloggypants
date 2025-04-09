f073b1a454e2ed56eb0104fe2b4204cb87369778
orion <oletizi@mac.com>
2025-04-09 10:25:12 -0700

    snarf works

---
+++ src/snarf/snarf.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs/promises'
+import {XMLParser} from 'fast-xml-parser'
+import path from 'node:path'
+
+const outdir = './build'
+
+async function main() {
+    try {
+        await fs.stat(outdir)
+    } catch (e) {
+        await fs.mkdir(outdir)
+    }
+
+    let sitemap = (await fs.readFile('sitemap.xml')).toString()
+    const doc = new XMLParser().parse(sitemap, {})
+    const urlset = doc.urlset
+    for (const i of urlset.url) {
+        const url = new URL(i.loc)
+        const outfile = path.join(outdir, url.pathname + 'index.html')
+        const dir = path.dirname(outfile)
+        try {
+            await fs.stat(dir)
+        } catch (e) {
+            await fs.mkdir(dir, {recursive: true})
+        }
+
+        try {
+            await fs.stat(outfile)
+            console.log(`CACHE HIT: ${outfile}`)
+        } catch (e) {
+            console.log(`CACHE MISS: ${outfile}`)
+            console.log(`fetching ${url}`)
+            const res = await fetch(url)
+            if (res.ok) {
+                console.log(`  ok. Will save to ${outfile}`)
+                await fs.writeFile(outfile, await res.text())
+            } else {
+                console.error(`  Barf: ${res.status} ${res.statusText}`)
+            }
+        }
+    }
+}
+
+
+main().then(() => console.log('Done.'))
---
+++ src/snarf/translate.ts
@@ -0,0 +1,42 @@
+import fs from 'fs/promises'
+import path from 'node:path'
+//<div id="site-content
+//  <h1 class="c-heading u-font-h2 u-color-light">
+//                     <p class="u-fz-h6 u-color-light">
+//                         <time class="entry__published" datetime="2023-07-05T06:00:00-07:00">July 5, 2023</time>
+//                         <span class="entry__separator">|</span> <span>Author:  Jimmy Song</span></p>
+//                     <div class="o-button-group"></div>
+// <div class="entry__content">
+
+const sourcedir = 'build'
+
+async function main() {
+    const todo = [sourcedir]
+    while (todo.length > 0) {
+        const item = todo.pop()
+        if (item) {
+            try {
+                const stats = await fs.stat(item)
+                if (stats.isDirectory()) {
+                    for (const child of await fs.readdir(item)) {
+                        todo.push(path.join(item, child))
+                    }
+                } else {
+                    if (stats.isFile() && item.endsWith('index.html')) {
+                        console.log(`Got one: ${item}`)
+                    } else {
+                        console.log(`Not sure what this is: ${item}`)
+                    }
+                }
+            } catch (e) {
+                // huh.
+                console.error(e)
+            }
+        }
+    }
+
+}
+
+main().then(() => {
+    console.log('Done.')
+})
