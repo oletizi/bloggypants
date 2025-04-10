d1f6ca8300ac7e35edd6ab23992db982b52cd168
orion <oletizi@mac.com>
2025-04-10 15:42:49 -0700

    translate.ts * breaking out configurable behavior into separate chunks

--- src/snarf/translate.ts
+++ src/snarf/translate.ts
@@ -3,19 +3,20 @@ import path from 'node:path'
 import {format} from 'date-fns'
 import {JSDOM} from 'jsdom'
 import TurndownService from 'turndown'
-//<div id="site-content
-//  <h1 class="c-heading u-font-h2 u-color-light">
-//                     <p class="u-fz-h6 u-color-light">
-//                         <time class="entry__published" datetime="2023-07-05T06:00:00-07:00">July 5, 2023</time>
-//                         <span class="entry__separator">|</span> <span>Author:  Jimmy Song</span></p>
-//                     <div class="o-button-group"></div>
-// <div class="entry__content">
+
 const width = `100%`
 const sourcedir = path.join('build', 'snarfed')
+const targetdir = path.join('src', 'pages')
 const turndownService = new TurndownService()
+const layoutPath = path.join('..', '..', '..', 'layouts', 'layout.astro')
+const figureImport = 'import {Figure} from "../../../components/Figure.tsx"'
+
+/**
+ * Special handling for <figure>
+ */
 turndownService.addRule('figure', {
     filter: 'figure',
-    replacement: function (_content, node: Node, _options){
+    replacement: function (_content, node: Node, _options) {
         const e = node as Element
         const img = e.querySelector('img')
         const caption = e.querySelector('figcaption')?.textContent
@@ -24,25 +25,62 @@ turndownService.addRule('figure', {
         return `<Figure src="${src}" width={"${width}"} caption="${caption}"/>`
     }
 })
+
+/**
+ * Special handling for <img>
+ */
 turndownService.addRule('img', {
     filter: 'img',
     replacement: function (_content, node: Node, _options) {
         const e = node as Element
         const src = e.getAttribute('src')
         const alt = e.hasAttribute('alt') ? e.getAttribute('alt') : ''
-        const c = e.getAttribute('class') ? e.getAttribute('class') : ''
-
-        // @ts-ignore
-        if (c.includes('wp-post-image')) {
+        const clazz = e.getAttribute('class') ? e.getAttribute('class') : ''
+        if (clazz && clazz.includes('wp-post-image')) {
             return ''
-        }else {
+        } else {
             return `<img class="" src="${src}" alt="${alt}">`
         }
     }
 })
 
+/**
+ * Special handling for <style>
+ */
+turndownService.addRule('style', {
+    filter: 'style',
+    replacement: function (_content, _node, _options) {
+        return ''
+    }
+})
+
+/**
+ * Translate the path to the source HTML file to the path of the destination markdown file
+ *
+ * @param sourceDir
+ * @param targetDir
+ * @param htmlPath
+ */
+function translateHtmlPathToMarkdownPath(sourceDir: string, targetDir: string, htmlPath: string) {
+    return htmlPath.replace(sourceDir, targetDir).replace('index.html', 'index.mdx')
+}
+
+/**
+ * Define where images are stored relative to the markdown file
+ *
+ * @param markdownPath
+ * @param imageName
+ */
+function translateImagePath(markdownPath: string, imageName: string): string {
+    return path.join(path.dirname(markdownPath), imageName)
+}
+
+/**
+ * Looks for html files in ${sourcedir} and applies the translate() function
+ */
 async function main() {
     const todo = [sourcedir]
+    const index = []
     while (todo.length > 0) {
         const item = todo.pop()
         if (item) {
@@ -54,9 +92,10 @@ async function main() {
                     }
                 } else {
                     if (stats.isFile() && item.endsWith('index.html')) {
-                        const translated = item.replace('index.html', 'index.mdx').replace('build/snarfed', 'src/pages')
-                        await translate(item, translated)
-
+                        // const markdownPath = item.replace('index.html', 'index.mdx').replace('build/snarfed', 'src/pages')
+                        const markdownPath = translateHtmlPathToMarkdownPath(sourcedir, targetdir, item)
+                        await translate(item, markdownPath)
+                        index.push(markdownPath)
                     } else {
                         console.log(`Not sure what this is: ${item}`)
                     }
@@ -70,6 +109,13 @@ async function main() {
 
 }
 
+/**
+ * Translates an html file snarfed from tetrate.io to markdown.
+ * See snarf.ts
+ *
+ * @param inpath the source html fils
+ * @param outpath the destination markdown/mdx file
+ */
 async function translate(inpath: string, outpath: string) {
     let title = ''
     let author = ''
@@ -82,6 +128,10 @@ async function translate(inpath: string, outpath: string) {
     } catch (e) {
         await fs.mkdir(outdir, {recursive: true})
     }
+
+    //
+    // Find H1 tag and make it the title
+    //
     const html = (await fs.readFile(inpath)).toString()
     const dom = new JSDOM(html)
     const doc = dom.window.document
@@ -91,7 +141,9 @@ async function translate(inpath: string, outpath: string) {
     }
     console.log(`  TITLE: ${title}`)
 
-
+    //
+    // Find the <time.entry__published> tag and make it the date
+    //
     const published = doc.querySelector('time.entry__published')
     if (published && published.hasAttribute('datetime')) {
         const datetime = published.getAttribute('datetime')
@@ -101,6 +153,9 @@ async function translate(inpath: string, outpath: string) {
     }
     console.log(`  DATE : ${date}`)
 
+    //
+    // Find and parse the <span> containing the byline and make it the author
+    //
     const spans = doc.querySelectorAll('#site-content p span')
     for (const span of spans) {
         if (span.textContent && span.textContent.toLowerCase().includes('author')) {
@@ -111,8 +166,15 @@ async function translate(inpath: string, outpath: string) {
         }
     }
     console.log(`  AUTHOR: ${author}`)
+
+    //
+    // Find <div.entry__content> and transform it to markdown
+    //
     const entry = doc.querySelector('div.entry__content')
     if (entry) {
+        //
+        // Download images from tetrate.io
+        //
         const images = entry.querySelectorAll('img')
         if (images) {
             for (const image of images) {
@@ -121,8 +183,9 @@ async function translate(inpath: string, outpath: string) {
                 if (src && src.startsWith('/')) {
                     src = 'https://tetrate.io' + src
                     const imageUrl = new URL(src)
-                    let filename = path.basename(imageUrl.pathname)
-                    const outfile = path.join(outdir.replace('src/pages', 'public'), filename)
+                    let imageName = path.basename(imageUrl.pathname)
+                    // const outfile = path.join(outdir.replace('src/pages', 'public'), filename)
+                    const outfile = translateImagePath(outpath, imageName)
                     try {
                         await fs.stat(outfile)
                         console.log(`  IMAGE CACHE HIT: ${outfile}`)
@@ -151,16 +214,20 @@ async function translate(inpath: string, outpath: string) {
                 }
             }
         }
+
+        //
+        // Convert html to markdown
+        //
         let markdown = turndownService.turndown(entry.innerHTML)
         markdown =
             `---\n` +
-            `layout: ../../../layouts/layout.astro\n` +
+            `layout: ${layoutPath}\n` +
             `title: ${title}\n` +
             `author: ${author}\n` +
             `date: ${format(date, 'yyyy-MM-dd')}\n` +
             `featuredImage: ${featuredImage}\n` +
             `---\n` +
-            'import {Figure} from "../../../components/Figure.tsx"\n\n' +
+            `${figureImport}\n\n` + // mdx parser seems to want a double newline after imports
             markdown
 
         await fs.writeFile(outpath, markdown)
